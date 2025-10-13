285f0940de08e94625daf5885fcee159055fa7d4
Ubuntu <ubuntu@ip-172-31-20-179.ap-southeast-1.compute.internal>
2025-10-13 08:01:55 +0000

    test

---
+++ Docker/task1/lifehub/go/go.mod
@@ -0,0 +1,3 @@
+module lifehub
+
+go 1.22.2
--- Docker/task1/lifehub/go/main.go
+++ Docker/task1/lifehub/go/main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello from Go running on port 8083!")
+        fmt.Fprintf(w, "Hello from Go running on port 8086!")
     })
 
-    fmt.Println("Server starting on port 8083...")
-    http.ListenAndServe(":8083", nil)
+    fmt.Println("Server starting on port 8086...")
+    http.ListenAndServe(":8086", nil)
 }
